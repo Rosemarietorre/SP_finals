806fa82aea08820c25bc923db1b303f31a1f6604
Rosemarietorre <161552057+Rosemarietorre@users.noreply.github.com>
2024-12-14 01:01:32 +0800

    Add files via upload

---
+++ servicepage/servicepage.php
@@ -0,0 +1,57 @@
+<?php
+// Include header and navigation (if necessary)
+include('header.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="service.css">
+    <title>Service Page</title>
+
+</head>
+<body>
+
+    <div class="wrapper">
+        <h1> Our Services </h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo alias sequi error excepturi iste consectetur bus isquam amet, sunt quae non? Iure.</p>
+
+        <div class="content-box">
+           <div class="card">
+             <i class="bx bx-bar-book-alt bx-md"></i>
+             <h2> Blog</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exex quia et. Nam sunt laborum libero.</p>
+           </div> 
+
+           <div class="card">
+             <i class="bx bx-pen-alt bx-md"></i>
+             <h2> Blog</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exex quia et. Nam sunt laborum libero.</p>
+           </div> 
+
+           <div class="card">
+             <i class="bx bx-book-alt bx-md"></i>
+             <h2> Blog</h2>
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed exex quia et. Nam sunt laborum libero.</p>
+           </div> 
+
+           
+        </div>
+
+    </div>
+    
+</body>
+</html>
+
+
+
+<?php
+// Include footer
+include('footer.php');
+?>
+
+
+
+
